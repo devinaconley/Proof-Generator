1593c51680cca893e0b6cea7d69b3b94ad4234b0
Devin Conley <devinconley@jhu.edu>
2016-12-29 15:25:17 -0500

    Parse given and target equations and symbol set from command line string arguments

--- run.py
+++ run.py
@@ -1,51 +1,89 @@
 # Driver script for testing ProofGenerator
 
 import sympy
+from sympy.parsing.sympy_parser import parse_expr
+import argparse
+
 from src.NodeHandler import NodeHandler
 
 # Quick POC for simple graph style search
+def main( ) :
+	# Operators and modifiers of interest
+	operators = [sympy.Mul, sympy.Add]  # , sympy.Pow]
+	modifiers = [sympy.sin, sympy.cos]  # , sympy.exp]
 
-x, y = sympy.symbols( 'x y' )
+	# Pull command line args
+	args = ParseArguments( )
 
-operators = [sympy.Mul, sympy.Add]#, sympy.Pow]
-symbolsOrig = [x, y]
-modifiers = [sympy.sin, sympy.cos]#, sympy.exp]
+	# Parse into left and right expressions
+	sGivenLeft, sGivenRight = args['given'].split( '=' )
+	givenLeft = parse_expr( sGivenLeft )
+	givenRight = parse_expr( sGivenRight )
 
-maxDepth = 1000
+	sProveLeft, sProveRight = args['prove'].split( '=' )
+	proveLeft = parse_expr( sProveLeft )
+	proveRight = parse_expr( sProveRight )
 
-symbols = []
-symbols.extend( symbolsOrig )
-for m in modifiers :
-	for s in symbolsOrig :
-		symbols.append( m( s ) )
+	symbolsOrig = givenLeft.free_symbols | givenRight.free_symbols | proveLeft.free_symbols | proveRight.free_symbols
 
-# Given left == right
-givenLeft = sympy.sin( x ) / sympy.cos( x ) + sympy.cos( x ) / sympy.sin( x )
-givenRight = y / (sympy.cos( x ) * sympy.sin( x ))
+	print symbolsOrig
 
-# Prove y == 1
-proveLeft = 1
-proveRight = y
 
-handler = NodeHandler( )
-handler.Add( givenLeft, givenRight )
+	symbols = []
+	symbols.extend( symbolsOrig )
+	for m in modifiers :
+		for s in symbolsOrig :
+			symbols.append( m( s ) )
 
-k = 0
-while k < maxDepth and handler :
-	tempL, tempR = handler.Pop( )
-	k += 1
-	print tempL, ' = ', tempR
-	if (tempL == proveLeft and tempR == proveRight) or (tempL == proveRight and tempR == proveLeft) :
-		break
-	# Add simplified and expanded to queue (partial variations of these?)
-	handler.Add( sympy.simplify( tempL ), sympy.simplify( tempR ) )
-	handler.Add( sympy.expand( tempL ), sympy.expand( tempR ) )
+	print symbols
 
-	# iterate through operators
-	for op in operators :
-		for sym in symbols :
-			handler.Add( op( tempL, sym ), op( tempR, sym ) )
 
-print len( handler.queue )
+	# Testing cost function
+
+
+
+	#  Do search
+
+	maxDepth = 1000
+
+	handler = NodeHandler( )
+	handler.Add( givenLeft, givenRight )
+
+	k = 0
+	while k < maxDepth and handler :
+		tempL, tempR = handler.Pop( )
+		k += 1
+		print tempL, ' = ', tempR
+		if (tempL == proveLeft and tempR == proveRight) or (tempL == proveRight and tempR == proveLeft) :
+			break
+		# Add simplified and expanded to queue (partial variations of these?)
+		handler.Add( sympy.simplify( tempL ), sympy.simplify( tempR ) )
+		handler.Add( sympy.expand( tempL ), sympy.expand( tempR ) )
+
+		# iterate through operators
+		for op in operators :
+			for sym in symbols :
+				handler.Add( op( tempL, sym ), op( tempR, sym ) )
+
+	print('Expanded nodes: ' + str( k ))
 
 # Need some distance metric to implement a priority queue
+def Cost( ) :
+	return 0
+
+# Command Line Arguments
+def ParseArguments( ) :
+	# Define arguments
+	parser = argparse.ArgumentParser( )
+	parser.add_argument( '-g', '--given', help='Given or starting statement',
+						 default='sin(x)/cos(x) + cos(x)/sin(x) = y/( cos(x) + sin(x) )' )
+	parser.add_argument( '-p', '--prove', help='Statement to prove',
+						 default='1 = y' )
+
+	# Parse arguments and return
+	args = vars( parser.parse_args( ) )
+
+	return args
+
+if __name__ == '__main__' :
+	main( )
